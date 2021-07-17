e86f8f4b288b84e80005dc5a4d795927cb33549b
ViacheslavSosnovskiy <saintstill333@gmail.com>
2021-07-17 18:15:14 +0300

    finish 1 task

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const sum = (a, b) => a + b;
+sum(2, 3);
+
+class Hero {
+  constructor(name) {
+    this._name = name;
+  }
+  get name() {
+    return this._name;
+  }
+  set name(newName) {
+    this._name = newName;
+  }
+}
