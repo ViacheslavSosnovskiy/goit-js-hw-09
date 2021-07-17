b807840fccbb4198e99f8e1a267c0f6a07f81aac
ViacheslavSosnovskiy <saintstill333@gmail.com>
2021-07-17 17:00:46 +0300

    added files

---
+++ app.js
@@ -0,0 +1,8 @@
+// const Joi = requere("joi");
+const shortid = require("shortid");
+
+// const passwordSchema = Joi.string().min(3).max(10).alphanum();
+
+// console.log(passwordSchema.validate("qwedfwe"));
+
+console.log(shortid.generate());
