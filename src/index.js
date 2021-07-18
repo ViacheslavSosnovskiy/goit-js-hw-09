c72068c7e0035ee300ee010345aafc497a48101a
ViacheslavSosnovskiy <saintstill333@gmail.com>
2021-07-18 05:13:42 +0300

    learn webpack

--- src/index.js
+++ src/index.js
@@ -1,18 +1,4 @@
-import validatePassword from "./js/validate-password";
-console.log(validatePassword("qweqwe"));
-
-// ===== 1-й способ =====
-// import {
-//   fetchAllUsers,
-//   fetchUsersBuId,
-//   updateUsersBuId,
-//   x as value,
-//   y as name,
-// } from "./js/api-service";
-// console.log(fetchAllUsers);
-// console.log(value);
-// console.log(name);
-
-// ===== 2 способ =====
-import * as apiService from "./js/api-service";
-console.log(apiService);
+import name from "./module-1";
+import "./css/common.scss";
+console.log(name);
+console.log("это index.js, привет");
